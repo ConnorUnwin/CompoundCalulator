aafd6f117efd25e7a5ba3801317046203553563a
ConnorUnwin <contact@connorunwin.com>
2018-08-22 01:18:29 +0100

    moved compound routine in to a function

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import math
 import sys as system
 
-base_amount = float(system.argv[1])
-duration = int(system.argv[2])
-annaul_interest = float(int(system.argv[3]) / 100)
-year = 0
 
 def truncate(number, digits) -> float:
     stepper = pow(10.00, digits)
     return math.trunc(stepper * number) / stepper
 
+def compound():
+    base_amount = float(system.argv[1])
+    duration = int(system.argv[2])
+    annaul_interest = float(int(system.argv[3]) / 100)
+    year = 0
 
-while year < duration:
-    year += 1
-    compound_amount = base_amount * annaul_interest
-    base_amount += compound_amount
-    print("Year {} £{}".format(year, truncate(base_amount, 2)))
+    while year < duration:
+        year += 1
+        compound_amount = base_amount * annaul_interest
+        base_amount += compound_amount
+        print("Year {} £{}".format(year, truncate(base_amount, 2)))
+
+compound()
